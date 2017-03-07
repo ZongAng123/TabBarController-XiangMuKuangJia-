7d1c2868a8a7d0c4c0cbc720e3a9b8e19463d03b
ZongAng123 <zongang15@yahoo.com>
2017-03-07 10:25:58 +0800

    Initial Commit

---
+++ 自定义TabBarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义TabBarController
+//
+//  Created by 纵昂 on 2017/3/7.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义TabBarController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自定义TabBarController
+//
+//  Created by 纵昂 on 2017/3/7.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
